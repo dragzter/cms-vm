d8c43e1dd58fb00e59e0f8b13c9268584a096d83
vm <vlad@q.local>
2018-09-21 00:09:24 -0400

    debug

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -41,19 +41,11 @@
                 </li>
                 <li><a href="#">Category Name</a>
                 </li>
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
             </ul>
         </div>
         <!-- /.col-lg-6 -->
         <div class="col-lg-6">
             <ul class="list-unstyled">
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
                 <li><a href="#">Category Name</a>
                 </li>
                 <li><a href="#">Category Name</a>
